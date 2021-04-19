2aa0914b3765902799dcea416a8dca0e6a978450
A-558 <81339780+A-558@users.noreply.github.com>
2021-04-19 18:51:32 +0530

    Add files via upload

---
+++ submit.js
@@ -0,0 +1,30 @@
+console.log('js started');
+
+var password ='';
+var repassword = '';
+
+function SU(){
+    password = document.getElementById('pswd').value;
+    repassword = document.getElementById('pw').value;
+
+   
+
+    if( password=='2233445566'){
+        document.getElementById('result').innerHTML = 'sussesfully singned up'
+        document.getElementById('result').style.color = ' green';
+        console.log('sucessfully signed up');
+    }
+    else if(  repassword!= '2233445566'){
+        document.getElementById('result').innerHTML = 'plz check the repassword';
+        document.getElementById('result').style.color = 'red';
+
+    }
+    else{
+        console.log('Just try 2233445566 AS your password');
+        document.getElementById('result').innerHTML = 'Just try 2233445566 AS your password';
+        document.getElementById('result').style.color = 'red';
+    }
+    return false;
+}
+
+console.log('js ended')
